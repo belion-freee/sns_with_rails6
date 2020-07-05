877ef6ff469f222d3271b9d285d952ad844ffb75
belion-freee <belion.freee@gmail.com>
2020-07-05 12:32:33 +0900

    setup rspec

---
+++ spec/support/spec_support.rb
@@ -0,0 +1,10 @@
+module SpecSupport
+  # Used when giving a unique name
+  def timestamp!(timestamp = Time.now.to_i)
+    @timestamp = timestamp
+  end
+
+  def timestamp
+    @timestamp
+  end
+end
